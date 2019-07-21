676ea31921865f845df019bb96261098dbc819a4
haalto <aaltonen.hannes@gmail.com>
2019-07-21 21:55:53 +0300

    added frontend build version

---
+++ frontend/build/precache-manifest.1c2f17727c933cdf496755e967ffc546.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f60b281232b5248c88a28e23c49d2fab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d9962415785a770c863",
+    "url": "/static/js/2.818c1384.chunk.js"
+  },
+  {
+    "revision": "8a9471390506cc8d1381",
+    "url": "/static/js/main.703fe5e6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
